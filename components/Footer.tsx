49e581ca765dd16f433109bc8a6e7074ab26e2ac
Kweku Duah Owusu Asamoah <asamoahduah101@gmail.com>
2025-10-20 13:01:13 +0000

    Remove unused Button import from Footer component for cleaner code.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,5 @@
 'use client';
 import React from 'react';
-import Button from '@/components/ui/Button';
 import LinkButton from './ui/LinkButton';
 import Image from 'next/image';
 import Link from 'next/link';
